4e3a765a79e84f4b140a19ce22f873f410500169
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-10-24 15:36:50 -0500

    feat: allow shuffling more than one deck

--- src/deck.rs
+++ src/deck.rs
@@ -27,8 +27,8 @@ impl Deck {
 		Deck { cards }
 	}
 
-	pub(crate) fn shuffle(&self) -> Self {
-		let deck = Deck::new(1);
+	pub(crate) fn shuffle(&self, deck_count: u8) -> Self {
+		let deck = Deck::new(deck_count);
 		let mut cards = deck.cards;
 		cards.shuffle(&mut thread_rng());
 
