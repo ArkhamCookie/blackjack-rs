cc36e0bcdbba0a2613364dafe3d924e5399eeb15
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-10-24 17:33:55 -0500

    docs: improve dealing comments

--- src/game.rs
+++ src/game.rs
@@ -6,19 +6,19 @@ pub(crate) fn deal(deck: Deck) -> (Vec<Card>, Vec<Card>) {
 	let mut dealer_hand = Vec::new();
 	let mut player_hand = Vec::new();
 
-	// Deal 1 card to player
+	// Deal 1 card to player (face up)
 	player_hand.push(cards[0]);
 	cards.remove(0);
 
-	// Deal 1 card to dealer
+	// Deal 1 card to dealer (face down)
 	dealer_hand.push(cards[0]);
 	cards.remove(0);
 
-	// Deal 1 card to player
+	// Deal 1 card to player (face up)
 	player_hand.push(cards[0]);
 	cards.remove(0);
 
-	// Deal 1 card to dealer
+	// Deal 1 card to dealer (face up)
 	dealer_hand.push(cards[0]);
 	cards.remove(0);
 
