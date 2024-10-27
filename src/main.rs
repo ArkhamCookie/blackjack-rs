edd7e03eb7cffa924ba65cc56db880f92320edd7
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-10-27 14:58:30 -0500

    docs: improve showing what steps are for

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,19 @@ fn main() {
     let mut player_hand = Vec::new();
     let mut dealer_hand = Vec::new();
 
-    // Deal 1 card to player (face up)
+    // [Setup] Deal 1 card to player (face up)
     player_hand.push(cards[0]);
     cards.remove(0);
 
-    // Deal 1 card to dealer (face down)
+    // [Setup] Deal 1 card to dealer (face down)
     dealer_hand.push(cards[0]);
     cards.remove(0);
 
-    // Deal 1 card to player (face up)
+    // [Setup] Deal 1 card to player (face up)
     player_hand.push(cards[0]);
     cards.remove(0);
 
-    // Deal 1 card to dealer (face up)
+    // [Setup] Deal 1 card to dealer (face up)
     dealer_hand.push(cards[0]);
     cards.remove(0);
 }
