1efb8d694f77572442bc8e11286dbf8ba8e89855
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-10-27 14:55:17 -0500

    refactor: move deal function to be part of the main function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use deck::Deck;
+
 mod card;
 mod deck;
 mod game;
 
 fn main() {
-    println!("Hello, world!");
+    let deck = Deck::shuffle(1);
+    let mut cards = deck.cards;
+    let mut player_hand = Vec::new();
+    let mut dealer_hand = Vec::new();
+
+    // Deal 1 card to player (face up)
+    player_hand.push(cards[0]);
+    cards.remove(0);
+
+    // Deal 1 card to dealer (face down)
+    dealer_hand.push(cards[0]);
+    cards.remove(0);
+
+    // Deal 1 card to player (face up)
+    player_hand.push(cards[0]);
+    cards.remove(0);
+
+    // Deal 1 card to dealer (face up)
+    dealer_hand.push(cards[0]);
+    cards.remove(0);
 }
