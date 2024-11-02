c7745c59bf1211371575b93aea43d29df48c6bdb
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-11-02 16:41:34 -0500

    chore: run `rustfmt`

--- src/main.rs
+++ src/main.rs
@@ -9,98 +9,103 @@ mod card;
 mod deck;
 
 fn main() {
-    let deck = Deck::shuffle(1);
-    let mut cards = deck.cards;
-    let mut player_hand = Vec::new();
-    let mut dealer_hand = Vec::new();
+	let deck = Deck::shuffle(1);
+	let mut cards = deck.cards;
+	let mut player_hand = Vec::new();
+	let mut dealer_hand = Vec::new();
 
-    // [Setup] Deal 1 card to player (face up)
-    player_hand.push(cards[0]);
-    cards.remove(0);
+	// [Setup] Deal 1 card to player (face up)
+	player_hand.push(cards[0]);
+	cards.remove(0);
 
-    // [Setup] Deal 1 card to dealer (face down)
-    dealer_hand.push(cards[0]);
-    cards.remove(0);
+	// [Setup] Deal 1 card to dealer (face down)
+	dealer_hand.push(cards[0]);
+	cards.remove(0);
 
-    // [Setup] Deal 1 card to player (face up)
-    player_hand.push(cards[0]);
-    cards.remove(0);
+	// [Setup] Deal 1 card to player (face up)
+	player_hand.push(cards[0]);
+	cards.remove(0);
 
-    // [Setup] Deal 1 card to dealer (face up)
-    dealer_hand.push(cards[0]);
-    cards.remove(0);
+	// [Setup] Deal 1 card to dealer (face up)
+	dealer_hand.push(cards[0]);
+	cards.remove(0);
 
-    // Basic display
-    println!("Dealer's Hand: Face Down, {:?}", dealer_hand[1].rank);
+	// Basic display
+	println!("Dealer's Hand: Face Down, {:?}", dealer_hand[1].rank);
 
-    println!("Your Hand: {:?}, {:?},", player_hand[0].rank, player_hand[1].rank);
+	println!(
+		"Your Hand: {:?}, {:?},",
+		player_hand[0].rank, player_hand[1].rank
+	);
 
-    // Get player's action
-    loop {
-        let options = vec!["Hit", "Stay"];
-        let answer = Select::new("Hit or stay?", options).prompt().expect("issue getting user action");
+	// Get player's action
+	loop {
+		let options = vec!["Hit", "Stay"];
+		let answer = Select::new("Hit or stay?", options)
+			.prompt()
+			.expect("issue getting user action");
 
-        if answer == "Stay" {
-            break
-        }
+		if answer == "Stay" {
+			break;
+		}
 
-        player_hand.push(cards[0]);
-        cards.remove(0);
-        
-        let mut score: u8 = 0;
+		player_hand.push(cards[0]);
+		cards.remove(0);
 
-        print!("Your hand: ");
-        for card in &player_hand {
-            score += Card::value(card);
-            print!("{:?}, ", card.rank);
+		let mut score: u8 = 0;
 
-            // Check for blackjack or bust
-            match score.cmp(&21) {
-                Ordering::Less => (),
-                Ordering::Equal => {
-                    println!("\nBlackjack!");
-                    return
-                }
-                Ordering::Greater => {
-                    println!("\nBusted!");
-                    return
-                }
-            }
-        }
-        println!();
-        // println!("{}", score);
-    }
+		print!("Your hand: ");
+		for card in &player_hand {
+			score += Card::value(card);
+			print!("{:?}, ", card.rank);
 
-    'dealer_action: loop {
-        let mut score: u8 = 0;
+			// Check for blackjack or bust
+			match score.cmp(&21) {
+				Ordering::Less => (),
+				Ordering::Equal => {
+					println!("\nBlackjack!");
+					return;
+				}
+				Ordering::Greater => {
+					println!("\nBusted!");
+					return;
+				}
+			}
+		}
+		println!();
+		// println!("{}", score);
+	}
 
-        print!("Dealer hand: ");
-        for card in &dealer_hand {
-            score += Card::value(card);
-            print!("{:?}, ", card.rank);
+	'dealer_action: loop {
+		let mut score: u8 = 0;
 
-            // Check for blackjack or bust
-            match score.cmp(&21) {
-                Ordering::Less => (),
-                Ordering::Equal => {
-                    println!("\nDealer gets blackjack!");
-                    return
-                }
-                Ordering::Greater => {
-                    println!("\nDealer busts!");
-                    return
-                }
-            }
-        }
-        println!();
-        // println!("{}", score);
+	    print!("Dealer hand: ");
+		for card in &dealer_hand {
+			score += Card::value(card);
+			print!("{:?}, ", card.rank);
 
-        if score >= 17 {
-            println!("Dealer stays.");
-            break 'dealer_action
-        }
+			// Check for blackjack or bust
+			match score.cmp(&21) {
+				Ordering::Less => (),
+				Ordering::Equal => {
+					println!("\nDealer gets blackjack!");
+					return;
+				}
+				Ordering::Greater => {
+					println!("\nDealer busts!");
+					return;
+				}
+			}
+		}
+		println!();
+		// println!("{}", score);
 
-        dealer_hand.push(cards[0]);
-        cards.remove(0);
-    }
+		if score >= 17 {
+			println!("Dealer stays.");
+			break 'dealer_action;
+		}
+
+		dealer_hand.push(cards[0]);
+		cards.remove(0);
+	}
 }
